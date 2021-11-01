53923036e923ce14a184f7fcb107325be950fb9d
Vitória Gonçalves <vitoriagcf@hotmail.com>
2021-11-01 15:33:21 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -37,10 +37,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		/*
-		 * try { Thread.sleep(3000L); } catch (InterruptedException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 */
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker obj = repository.findById(id).get();
