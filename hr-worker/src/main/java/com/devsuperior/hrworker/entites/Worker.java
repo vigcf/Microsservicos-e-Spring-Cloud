1b6bafb15bd34afae9e33ff51158ae37f75a9a4a
Vitória Gonçalves <vitoriagcf@hotmail.com>
2021-11-02 18:18:55 -0300

    hr-worker as a config server

--- hr-worker/src/main/java/com/devsuperior/hrworker/entites/Worker.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entites/Worker.java
@@ -2,34 +2,32 @@ package com.devsuperior.hrworker.entites;
 
 import java.io.Serializable;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double dailyIncome;
+	
+	public Worker() {
+	}
 
+	public Worker(Long id, String name, Double dailyIncome) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
 
 	public Long getId() {
 		return id;
@@ -55,4 +53,28 @@ public class Worker implements Serializable {
 		this.dailyIncome = dailyIncome;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
