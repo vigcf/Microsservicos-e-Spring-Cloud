7f568a18acec831325895cecd966295e817adb24
Vitória Gonçalves <vitoriagcf@hotmail.com>
2021-11-21 01:32:08 -0300

    Private Git repository

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -22,42 +22,42 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-
+	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-
+	
 	@Value("${test.config}")
 	private String testConfig;
-
+	
 	@Autowired
 	private Environment env;
-
+	
 	@Autowired
 	private WorkerRepository repository;
-
+	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
 		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
-	}
-
+	}		
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-	}
-
+	}	
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-
+		
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-
+		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-	}
+	}	
 }
