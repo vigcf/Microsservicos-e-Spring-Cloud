a11768a289ab501a46f3c24b46c5aea243319058
Vitória <vitoriagcf@hotmail.com>
2021-12-12 02:18:35 -0300

    update

--- hr-config-server/src/test/java/com/devsuperior/configserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/com/devsuperior/configserver/HrConfigServerApplicationTests.java
@@ -3,7 +3,9 @@ package com.devsuperior.configserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.devsuperior.hrconfigserver.HrConfigServerApplication;
+
+@SpringBootTest(classes = HrConfigServerApplication.class)
 class HrConfigServerApplicationTests {
 
 	@Test
